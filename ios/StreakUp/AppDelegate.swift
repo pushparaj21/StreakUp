109832ae21fab54ecb8e96f21d0995a1e6d89d49
pushparaj21 <singhpushparaj31@gmail.com>
2025-07-19 19:52:42 +0530

    Rename project from NativeGithub to StreakUp, updating all relevant configurations, file names, and references across Android and iOS platforms to reflect the new branding.

--- ios/StreakUp/AppDelegate.swift
+++ ios/StreakUp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "NativeGithub",
+      withModuleName: "StreakUp",
       in: window,
       launchOptions: launchOptions
     )
